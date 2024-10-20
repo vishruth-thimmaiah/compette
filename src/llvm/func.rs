d07a0eda62951369720798671849f24c1269dd76
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-10-21 00:55:53 +0530

    fix: order of exec while calling funcs with args

--- src/llvm/func.rs
+++ src/llvm/func.rs
@@ -6,7 +6,7 @@ use inkwell::context::Context;
 use inkwell::execution_engine::{ExecutionEngine, JitFunction};
 use inkwell::module::Module;
 use inkwell::types::BasicType;
-use inkwell::values::BasicValueEnum;
+use inkwell::values::{BasicValueEnum, PointerValue};
 use inkwell::OptimizationLevel;
 
 use crate::lexer::types::{Types, DATATYPE, OPERATOR};
@@ -19,13 +19,27 @@ use crate::parser::types::ParserTypes;
 
 type MainFunc = unsafe extern "C" fn() -> u32;
 
+struct FunctionStore<'ctx> {
+    name: String,
+    args: HashMap<String, PointerValue<'ctx>>,
+}
+
+impl FunctionStore<'_> {
+    fn new(name: String) -> Self {
+        Self {
+            name,
+            args: HashMap::new(),
+        }
+    }
+}
+
 pub struct CodeGen<'ctx> {
     pub context: &'ctx Context,
     pub builder: Builder<'ctx>,
     module: Module<'ctx>,
     execution_engine: ExecutionEngine<'ctx>,
     tokens: Vec<Box<dyn ParserType>>,
-    variables: RefCell<HashMap<String, BasicValueEnum<'ctx>>>,
+    variables: RefCell<Vec<FunctionStore<'ctx>>>,
 }
 
 impl<'ctx> CodeGen<'ctx> {
@@ -40,7 +54,7 @@ impl<'ctx> CodeGen<'ctx> {
             builder: context.create_builder(),
             execution_engine,
             tokens,
-            variables: RefCell::new(HashMap::new()),
+            variables: RefCell::new(Vec::new()),
         }
     }
 
@@ -68,28 +82,34 @@ impl<'ctx> CodeGen<'ctx> {
         }
     }
 
-    fn nested_codegen(&self, body: &Vec<Box<dyn ParserType>>, ret_type: &DATATYPE) {
+    fn nested_codegen(
+        &self,
+        body: &Vec<Box<dyn ParserType>>,
+        func_name: &str,
+        ret_type: &DATATYPE,
+    ) {
         for node in body {
             match node.get_type() {
                 ParserTypes::VARIABLE => {
                     let downcast_node = node.any().downcast_ref::<AssignmentParserNode>().unwrap();
-                    self.add_variable(downcast_node);
+                    self.add_variable(func_name, downcast_node);
                 }
                 ParserTypes::RETURN => {
                     let downcast_node = node.any().downcast_ref::<ReturnNode>().unwrap();
-                    self.add_return(downcast_node, ret_type);
+                    self.add_return(downcast_node, func_name, ret_type);
                 }
                 _ => todo!(),
             }
         }
     }
 
-    fn add_variable(&self, node: &AssignmentParserNode) {
+    fn add_variable(&self, func_name: &str, node: &AssignmentParserNode) {
         let alloc = self.new_ptr(node);
-        let build_alloc = self.builder.build_load(self.def_expr(&node.var_type), alloc, &node.var_name).unwrap();
-        self.variables
-            .borrow_mut()
-            .insert(node.var_name.clone(), build_alloc);
+        self.variables.borrow_mut().iter_mut().for_each(|x| {
+            if x.name == func_name {
+                x.args.insert(node.var_name.clone(), alloc);
+            }
+        });
 
         let value = node
             .value
@@ -98,11 +118,14 @@ impl<'ctx> CodeGen<'ctx> {
             .unwrap();
 
         self.builder
-            .build_store(alloc, self.add_expression(value, &node.var_type))
+            .build_store(alloc, self.add_expression(value, func_name, &node.var_type))
             .unwrap();
     }
 
     fn add_function(&self, node: &FunctionParserNode) {
+        self.variables
+            .borrow_mut()
+            .push(FunctionStore::new(node.func_name.clone()));
         let args = self.def_func_args(&node.args);
 
         let ret_type = node.return_type.as_ref().unwrap();
@@ -111,24 +134,25 @@ impl<'ctx> CodeGen<'ctx> {
 
         for (index, arg) in function.get_param_iter().enumerate() {
             arg.set_name(&node.args[index].0);
-            self.variables
-                .borrow_mut()
-                .insert(node.args[index].0.clone(), arg);
         }
 
         let basic_block = self.context.append_basic_block(function, "entry");
         self.builder.position_at_end(basic_block);
 
-        self.nested_codegen(&node.body, node.return_type.as_ref().unwrap());
+        self.nested_codegen(
+            &node.body,
+            &node.func_name,
+            node.return_type.as_ref().unwrap(),
+        );
     }
 
-    fn add_return(&self, node: &ReturnNode, ret_type: &DATATYPE) {
+    fn add_return(&self, node: &ReturnNode, func_name: &str, ret_type: &DATATYPE) {
         let ret_expr = node
             .return_value
             .any()
             .downcast_ref::<ExpressionParserNode>()
             .unwrap();
-        let ret_val = self.add_expression(ret_expr, ret_type);
+        let ret_val = self.add_expression(ret_expr, func_name, ret_type);
 
         self.builder.build_return(Some(&ret_val)).unwrap();
     }
@@ -136,6 +160,7 @@ impl<'ctx> CodeGen<'ctx> {
     fn add_expression(
         &self,
         node: &ExpressionParserNode,
+        func_name: &str,
         req_type: &DATATYPE,
     ) -> BasicValueEnum<'ctx> {
         let left_val = match node.left.get_type() {
@@ -144,11 +169,36 @@ impl<'ctx> CodeGen<'ctx> {
                 match value_parser_node.r#type {
                     Types::NUMBER => self.string_to_value(&value_parser_node.value, req_type),
 
-                    Types::IDENTIFIER => *self
-                        .variables
-                        .borrow()
-                        .get(value_parser_node.value.as_str())
-                        .expect("unknown variable."),
+                    Types::IDENTIFIER => {
+                        let vars = self
+                            .variables
+                            .borrow();
+                        let var_name = vars
+                            .iter()
+                            .filter(|x| x.name == func_name)
+                            .collect::<Vec<&_>>()[0]
+                            .args
+                            .get(value_parser_node.value.as_str());
+                        let res = {
+                            if let Some(var_name) = var_name {
+                                self.builder
+                                    .build_load(
+                                        self.def_expr(req_type),
+                                        *var_name,
+                                        &value_parser_node.value,
+                                    )
+                                    .unwrap()
+                            } else if let Some(func) = self.module.get_function(func_name) {
+                                func.get_params().iter().find(|x| {
+                                    x.get_name().to_str().unwrap() == value_parser_node.value
+                                }).unwrap().to_owned()
+                            }
+                            else {
+                                panic!("Invalid type");
+                            }
+                        };
+                        res
+                    }
                     _ => panic!("Invalid type"),
                 }
             }
@@ -164,7 +214,7 @@ impl<'ctx> CodeGen<'ctx> {
                 let params = function.get_params();
                 for (index, arg) in downcast_node.args.iter().enumerate() {
                     args.push(
-                        self.add_expression(arg, self.get_datatype(params[index]))
+                        self.add_expression(arg, func_name, self.get_datatype(params[index]))
                             .into(),
                     );
                 }
@@ -182,7 +232,7 @@ impl<'ctx> CodeGen<'ctx> {
         let right_val = {
             if let Some(right) = &node.right {
                 let right_expr = right.any().downcast_ref::<ExpressionParserNode>().unwrap();
-                self.add_expression(right_expr, req_type)
+                self.add_expression(right_expr, func_name, req_type)
             } else {
                 return left_val;
             }
