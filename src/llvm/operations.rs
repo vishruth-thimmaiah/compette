1d92c52ba9a2a1b356d945da60dd9c26ce8ea439
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-10-21 18:05:06 +0530

    fix: use unnamed vars for operations

--- src/llvm/operations.rs
+++ src/llvm/operations.rs
@@ -12,14 +12,14 @@ impl<'ctx> CodeGen<'ctx> {
             let left_int = left.into_int_value();
             let right_int = right.into_int_value();
             self.builder
-                .build_int_add(left_int, right_int, "main")
+                .build_int_add(left_int, right_int, "")
                 .unwrap()
                 .into()
         } else {
             let left_float = left.into_float_value();
             let right_float = right.into_float_value();
             self.builder
-                .build_float_add(left_float, right_float, "main")
+                .build_float_add(left_float, right_float, "")
                 .unwrap()
                 .into()
         }
@@ -34,14 +34,14 @@ impl<'ctx> CodeGen<'ctx> {
             let left_int = left.into_int_value();
             let right_int = right.into_int_value();
             self.builder
-                .build_int_sub(left_int, right_int, "main")
+                .build_int_sub(left_int, right_int, "")
                 .unwrap()
                 .into()
         } else {
             let left_float = left.into_float_value();
             let right_float = right.into_float_value();
             self.builder
-                .build_float_sub(left_float, right_float, "main")
+                .build_float_sub(left_float, right_float, "")
                 .unwrap()
                 .into()
         }
@@ -56,14 +56,14 @@ impl<'ctx> CodeGen<'ctx> {
             let left_int = left.into_int_value();
             let right_int = right.into_int_value();
             self.builder
-                .build_int_mul(left_int, right_int, "main")
+                .build_int_mul(left_int, right_int, "")
                 .unwrap()
                 .into()
         } else {
             let left_float = left.into_float_value();
             let right_float = right.into_float_value();
             self.builder
-                .build_float_mul(left_float, right_float, "main")
+                .build_float_mul(left_float, right_float, "")
                 .unwrap()
                 .into()
         }
@@ -78,14 +78,14 @@ impl<'ctx> CodeGen<'ctx> {
             let left_int = left.into_int_value();
             let right_int = right.into_int_value();
             self.builder
-                .build_int_signed_div(left_int, right_int, "main")
+                .build_int_signed_div(left_int, right_int, "")
                 .unwrap()
                 .into()
         } else {
             let left_float = left.into_float_value();
             let right_float = right.into_float_value();
             self.builder
-                .build_float_div(left_float, right_float, "main")
+                .build_float_div(left_float, right_float, "")
                 .unwrap()
                 .into()
         }
