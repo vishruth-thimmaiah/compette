c34724d1113c4fca54dcec448f4ad703e009dcd9
Vishruth-Thimmaiah <vishruththimmaiah@gmail.com>
2024-11-01 15:55:38 +0530

    refactor: move print(ln) to subdir io

---
+++ stdlib/src/io/mod.rs
@@ -0,0 +1,20 @@
+use std::{
+    ffi::{c_char, CStr},
+    io::Write,
+};
+
+#[no_mangle]
+extern "C" fn print(s: *const c_char) -> i32 {
+    let string = unsafe { CStr::from_ptr(s) };
+    std::io::stdout().write_all(string.to_bytes()).unwrap();
+    std::io::stdout().flush().unwrap();
+    return 0;
+}
+
+#[no_mangle]
+extern "C" fn println(s: *const c_char) -> i32 {
+    let string = unsafe { CStr::from_ptr(s) };
+    std::io::stdout().write_all(string.to_bytes()).unwrap();
+    std::io::stdout().write(b"\n").unwrap();
+    return 0;
+}
